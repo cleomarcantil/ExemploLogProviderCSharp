f61617915637840b19f4020c57305b637a789522
Cleomar Cantil <cleomarcantil@gmail.com>
2022-11-14 13:30:23 -0300

    Criado FileLoggerProvider.

--- ExemploLogProviderCSharp/Controllers/TesteController.cs
+++ ExemploLogProviderCSharp/Controllers/TesteController.cs
@@ -7,11 +7,33 @@ namespace ExemploLogProviderCSharp.Controllers
 	[ApiController]
 	public class TesteController : ControllerBase
 	{
+		private readonly ILogger<TesteController> logger;
+
+		public TesteController(ILogger<TesteController> logger)
+		{
+			this.logger = logger;
+		}
 
 		[HttpGet]
 		public string Teste()
 		{
+			logger.LogInformation($"Chamado método {nameof(Teste)}");
+
 			return "Teste";
+		}
+
+
+		[HttpPost]
+		public void TesteErro()
+		{
+			try
+			{
+				throw new Exception("Teste erro!");
+			}
+			catch (Exception ex)
+			{
+				logger.LogError(ex, $"Chamado método {nameof(TesteErro)}");
+			}
 		}
 
 	}
---
+++ ExemploLogProviderCSharp/Providers/FileLoggerProvider.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Concurrent;
+
+namespace ExemploLogProviderCSharp.Providers;
+
+[ProviderAlias("FileLogger")]
+internal class FileLoggerProvider : ILoggerProvider
+{
+	private readonly ConcurrentDictionary<string, FileLogger> _loggers = new();
+	private readonly IConfiguration configuration;
+
+	public FileLoggerProvider(IConfiguration configuration)
+	{
+		this.configuration = configuration;
+	}
+
+	public ILogger CreateLogger(string categoryName) =>
+		_loggers.GetOrAdd(categoryName, name => new FileLogger(name, configuration));
+
+	public void Dispose() => _loggers.Clear();
+
+
+	#region FileLogger
+
+	class FileLogger : ILogger
+	{
+		private readonly string _name;
+		private readonly IConfiguration configuration;
+
+		public FileLogger(string name, IConfiguration configuration)
+		{
+			this._name = name;
+			this.configuration = configuration;
+		}
+
+		public IDisposable BeginScope<TState>(TState state) => default;
+
+		public bool IsEnabled(LogLevel logLevel) => true;
+
+		public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+		{
+			if (!IsEnabled(logLevel))
+				return;
+
+			var linhas = new List<string>()
+				{
+					$"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{eventId.Id,2}: {logLevel,-12}] {_name}",
+					$"\t{formatter(state, exception)}",
+				};
+
+			if (exception is { } ex)
+			{
+				while (ex != null)
+				{
+					linhas.Add($"\t - {ex.GetType().Name}: {ex.Message}");
+					ex = ex.InnerException;
+				}
+			}
+
+			AppendToLogFile(linhas);
+		}
+
+		private static object _lockWriter = new object();
+		private void AppendToLogFile(IEnumerable<string> lines)
+		{
+			var logSubDir = configuration["LogStoragePaths"];
+
+			if (string.IsNullOrWhiteSpace(logSubDir))
+				return;
+
+			lock (_lockWriter)
+			{
+				var logDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, logSubDir);
+
+				if (!Directory.Exists(logDir))
+					Directory.CreateDirectory(logDir);
+
+				using var writer = File.AppendText(Path.Combine(logDir, $"registro-log-{DateTime.Now:yyyy-MM-dd}.log"));
+
+				try
+				{
+					foreach (var line in lines)
+						writer.WriteLine(line);
+				}
+				finally
+				{
+					writer.Close();
+				}
+			}
+		}
+
+	}
+
+	#endregion
+}
